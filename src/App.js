bc03c2dfba3464577016f7e25c6e7c2e6ad483c8
John Bede <johnbede2000@gmail.com>
2021-07-15 16:23:41 +0100

    basic form setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+import React, { useState } from 'react';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
+
 function App() {
-  return <p>Hello world!</p>;
+  const [selectedDate, handleDateChange] = useState(new Date());
+
+  return (
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <form>
+        <div>
+          <DatePicker value={selectedDate} onChange={handleDateChange} />
+        </div>
+        <div>
+          <label htmlFor="venuename">Venue Name: </label>
+          <input type="text" id="venuename" name="venuename"></input>
+        </div>
+        <div>
+          <label htmlFor="bandname">Band Name: </label>
+          <input type="text" id="bandename" name="bandname"></input>
+        </div>
+        <div>
+          <label htmlFor="bandlink">Band Link: </label>
+          <input type="text" id="bandlink" name="bandlink"></input>
+        </div>
+        <div>
+          <label htmlFor="city">Venue Name: </label>
+          <input type="text" id="city" name="city"></input>
+        </div>
+
+        <div>
+          <label htmlFor="link">Link for info / tickets: </label>
+          <input type="text" id="link" name="link"></input>
+        </div>
+        <div>
+          <label htmlFor="desc">Description (e.g. musicians): </label>
+          <input type="text" id="desc" name="desc"></input>
+        </div>
+        <div>
+          <label htmlFor="public">Public?</label>
+          <input type="checkbox" id="public" name="public"></input>
+        </div>
+
+        <button type="submit">SUBMIT</button>
+      </form>
+    </MuiPickersUtilsProvider>
+  );
 }
 
 export default App;
